c5cef645734578814e9a3ed5026032cafe5ad26f
padavalachoshmitha <padavalachoshmitha@gmail.com>
2026-07-30 18:49:04 +0530

    Add files via upload

---
+++ Week Programs/Week 1/Annotations.ts
@@ -0,0 +1,23 @@
+//Variable Annotations
+let cuisineName:string="Paradise!";
+let restaurantRating:number=5;
+let tasty:boolean=true;
+
+//Function parameter and Return Type Annotations
+function getReserved(title:string,stars:number):string{
+    return '${title} was serving a delicious food with restaurantRating of ${stars}.';
+}
+
+//Array Annotations
+let menuItems:string[]=[" Chicken Biryani","Ghobi 65","gulab jamun","Mutton ghee roast"];
+
+//Using the Annotated variables and functions
+console.log("Cuisine Name:", cuisineName);
+console.log("Rating:", restaurantRating);
+console.log("Is Delicious:", tasty);
+console.log(getReserved(cuisineName, restaurantRating));
+
+console.log("Food Menu:");
+for (let food of menuItems) {
+    console.log(food);
+}
---
+++ Week Programs/Week 1/datatype.ts
@@ -0,0 +1,55 @@
+//String programs
+let message1: string = "Hello, Ranii";
+console.log(str1);
+let message2: string = "How are you?";
+console.log(str2);
+let message3: string = "Have a good day.";
+console.log(str3+"\n");
+
+//number programs
+let inputNumber1: number = 10;
+console.log(num1);
+let inputNumber2: number = 25.5;
+console.log(num2);
+let inputNumber3: number = -100;
+console.log(num3+"\n");
+
+//Boolean programs
+let bool1: boolean = true;
+console.log(bool1);
+let bool2: boolean = 65<75;
+console.log(bool2);
+let bool3: boolean = 79>86;
+console.log(bool3+"\n");
+
+//Null programs
+let inputData1: null = null;
+console.log(data1);
+let inputData2: null = null;
+console.log(data2);
+let inputData3: null = null;
+console.log(data3+"\n");
+
+//undefined programs
+let dynamicValue1: undefined = undefined;
+console.log(value1);
+let dynamicValue2: undefined;
+console.log(value2);
+let dynamicValue3: undefined = undefined;
+console.log(value3+"\n");
+
+//BigInt programs
+let big1: bigint = 12345678901234567890n;
+console.log(big1);
+let big2: bigint = 98765432109876543210n;
+console.log(big2);
+let big3: bigint = 1000000000000000000000n;
+console.log(big3+"\n");
+
+//Symbol programs
+let sym1: symbol = Symbol("id");
+console.log(sym1);
+let sym2: symbol = Symbol("user");
+console.log(sym2);
+let sym3: symbol = Symbol("employee");
+console.log(sym3);
