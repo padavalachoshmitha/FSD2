ce1c8870058f947cdcb68caa873eb9d3bd3e8c83
padavalachoshmitha <padavalachoshmitha@gmail.com>
2026-08-08 20:14:25 +0530

    Update generic_constraint.ts

--- Week Programs/Week 4/generic_constraint.ts
+++ Week Programs/Week 4/generic_constraint.ts
@@ -13,7 +13,7 @@ function showLength<T extends Countable>(item: T): number {
 }
 
 // Works
-console.log(showLength("Ranisumansvi"));
+console.log(showLength("Choshmitha"));
 
 console.log(showLength([10, 20, 30, 40]));
 
